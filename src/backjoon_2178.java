9c492b9d1f15f63993dd79c0029b64f936561628
ajh94 <ajh94@DESKTOP-A17L8RN>
2022-10-25 23:10:31 +0900

    1

--- src/backjoon_2178.java
+++ src/backjoon_2178.java
@@ -1,9 +1,12 @@
-
+/*
+ * 백준 2178 미로 탐색
+ * https://www.acmicpc.net/problem/2178
+ * 
+ */
 public class backjoon_2178 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
